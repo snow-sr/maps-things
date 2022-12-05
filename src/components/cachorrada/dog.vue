0d704145c84439dd202b95026b864b992beabf15
snow-sr <joaofelipi877@gmail.com>
2022-12-04 22:25:44 -0300

    Add: Maps system, markers system, dog retrieving system, LATandLON map of IFC and every point of feeding

--- src/components/cachorrada/dog.vue
+++ src/components/cachorrada/dog.vue
@@ -1,14 +1,15 @@
 <!-- eslint-disable-next-line vue/multi-word-component-names -->
 <template>
   <main :style="style">
-    <div class="icn">
+    <div class="icn" :style="pos">
       <h1>{{ name }}</h1>
+      <h3>{{ newPos }}</h3>
     </div>
-    <span>A</span>
   </main>
 </template>
 
 <script>
+import { map } from "@/plugins/map.js";
 export default {
   props: {
     name: {
@@ -16,20 +17,55 @@ export default {
       required: true,
     },
     position: {
-      type: Array,
+      type: String,
       required: true,
     },
   },
 
+  data() {
+    return {
+      newPos: [],
+      screenSizeX: window.innerWidth,
+      screenSizeY: window.innerHeight,
+    };
+  },
+
+  mounted() {
+    if (this.position in map) {
+      let newPos = map[this.position][this.screenSize];
+      console.log("Position:" + this.position, "ScreenSize:" + this.screenSize);
+      console.log(newPos);
+      console.log(this.name);
+      this.newPos = newPos;
+    }
+  },
+
   computed: {
     style() {
-      return `transform: translate(${this.position[0]}px, ${this.position[1]}px)`;
+      return `transform: translate(${this.newPos[0]}em, ${this.newPos[1]}em)`;
+    },
+    screenSize() {
+      if (this.screenSizeX < 500 && this.screenSizeY < 500) {
+        return "sm";
+      } else if (this.screenSizeX < 1000 && this.screenSizeY < 1000) {
+        return "md";
+      } else if (this.screenSizeX < 1500 && this.screenSizeY < 1500) {
+        return "lg";
+      } else {
+        return "lg";
+      }
+    },
+    pos() {
+      return `content: "ourPosition: ${this.position}}"`;
     },
   },
 };
 </script>
 
 <style scoped>
+* {
+  opacity: 1.1;
+}
 span {
   font-size: 14pt;
   margin-left: 5px;
@@ -38,28 +74,30 @@ span {
 .icn {
   display: flex;
   align-items: center;
+  flex-direction: row;
   justify-content: center;
   background-color: #7700ff;
   border-radius: 50%;
-  width: 70px;
-  height: 70px;
+  width: 30px;
+  height: 30px;
 }
 
 main {
+  color: "black";
   border-radius: 50%;
-  color: #eef;
   display: flex;
   align-items: center;
+  flex-direction: row;
   cursor: pointer;
-  background-color: #ff00ff;
-  width: 70px;
-  height: 70px;
+  background-color: #0a020a;
+  width: 30px;
+  height: 30px;
 }
-
+/* 
 main:hover {
   transition: 500ms;
   height: 70px;
   width: 200px;
   border-radius: 50px;
-}
+} */
 </style>
--- src/views/Comentarios.vue
+++ src/views/Comentarios.vue
@@ -4,14 +4,14 @@ import dog from "@/components/cachorrada/dog.vue";
 import axios from "axios";
 import { mapStores, mapState } from "pinia";
 import { useAuthStore } from "@/stores/auth";
+import leaflet from "leaflet";
+import { onMounted } from "@vue/runtime-core";
+import { map } from "@/plugins/map.js";
+
 export default {
   components: { Comentarios, dog },
   data() {
     return {
-      x : [200,400],
-      y : [200,400],
-      z : [150,400],
-      a : [150,500],
       tagA: {},
       comentarios: [],
       comentario: {
@@ -38,33 +38,51 @@ export default {
       const comentarios = await axios.get("http://localhost:8000/coments/");
       this.comentarios = comentarios.data;
     },
-    async getTagA() {
-      const tagA = await axios.get("http://127.0.0.1:8000/tags/?ID=1");
-      this.tagA = tagA.dataA
-    }
   },
   async created() {
     await this.getAllComments();
     await this.getTagA();
+  },
+  setup() {
+    console.log("setup");
+    onMounted(async () => {
+      const tags = await axios.get("http://localhost:8000/tags/");
+
+      let myMap = leaflet.map("map").setView([-26.394672, -48.738026], 18);
+      leaflet
+        .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          maxZoom: 19,
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        })
+        .addTo(myMap);
+
+      tags.data.forEach((dog) => {
+        //pop up
+        let popup = leaflet
+          .popup()
+          .setContent(
+            `<h1>${dog.cachorro.nome}</h1><p>Estou no bloco: ${dog.local.local}</p>`
+          )
+          .setLatLng(map[dog.local.local])
+          .addTo(myMap);
+      });
+    });
   },
 };
 </script>
 <template>
   <div class="all">
     <!-- main -->
-    <main class="mapa">
-      <dog name="FMÇ" :position="x"></dog>
-      <dog name="MAI" :position="y"></dog>
-      <dog name="CHU" :position="z"></dog>
-      <dog name="FOR" :position="a"></dog>
-    </main>
+    <div id="map" class="map" @click=""></div>
     <!-- main -->
     <div class="corpo">
       <div class="comentarios">
         <Comentarios
           v-for="comentario in comentarios"
           :key="comentario.id"
-          :comentarios="comentario" />
+          :comentarios="comentario"
+        />
       </div>
     </div>
   </div>
@@ -76,36 +94,43 @@ export default {
         style="padding: 4px"
         placeholder="escreva seu comentario
           "
-        v-model="comentario.texto" />
+        v-model="comentario.texto"
+      />
       <div class="submit">
         <button
           v-on:click.prevent="addComment"
           type="submit"
-          class="btn btn-primary">
+          class="btn btn-primary"
+        >
           Enviar
         </button>
-        <button
+        <!-- <button
           v-on:click.prevent="getTagA"
           type="submit"
-          class="btn btn-primary">
+          class="btn btn-primary"
+        >
           getTag
         </button>
         <div>
           {{ tagA }}
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.map {
+  height: 100vh;
+  width: 100%;
+}
 .all {
   display: flex;
 }
 .mapa {
   border-radius: 20px 20px 0 0;
   background-image: url("../assets/img/ifcmapatt.png");
-  background-size: cover;
+  background-size: contain;
   background-repeat: no-repeat;
   background-position: center;
   height: 90vh;
